444ec2b1110e0ce1ad2a032feb305871486064aa
Dânison dos Santos <danison.santos00@gmail.com>
2024-02-12 10:53:01 -0300

    refact: selection btn

--- src/app/components/menu-left/menu-left.component.ts
+++ src/app/components/menu-left/menu-left.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-left',
   templateUrl: './menu-left.component.html',
-  styleUrls: ['./menu-left.component.scss']
+  styleUrls: ['./menu-left.component.scss'],
 })
-export class MenuLeftComponent {
+export class MenuLeftComponent implements OnInit {
+  ngOnInit(): void {}
 
+  log(span: EventTarget | null) {
+    const btns = document.getElementsByClassName(
+      'btn'
+    ) as HTMLCollectionOf<HTMLSpanElement>;
+
+    for (let i = 0; i < btns.length; i++) {
+      btns[i].style.backgroundColor = '';
+    }
+
+    if (span instanceof HTMLSpanElement) {
+      span.style.backgroundColor = '#2b2e44';
+    }
+  }
 }
